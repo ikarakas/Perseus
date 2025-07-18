9ba81764f4c91c117aa60e3d8245f1a079ccd5ec
Ilker M. Karakas <ilker.murat.karakas@gmail.com>
2025-07-18 12:58:10 +0300

    Remove examples directory from tracking and add to .gitignore

--- examples/docker-go-app/go.mod
+++
@@ -1,3 +0,0 @@
-module goapp
-
-go 1.21
--- examples/java-nato-library/_int/nato/ncia/natxmlib/types/NamedField.java
+++
@@ -1,9 +0,0 @@
-package _int.nato.ncia.natxmlib.types;
-
-/**
- *
- * @author mike
- */
-public interface NamedField<K> extends Field<K> {
-    String getName();
-}
